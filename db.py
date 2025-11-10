b37fecf9426488797aa6184c20c378f91255b6a0
PistisNdg <pistisndunge@gmail.com>
2025-11-10 11:29:42 +0100

    Update db.py

--- db.py
+++ db.py
@@ -14,18 +14,10 @@ from firebase_admin import messaging
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
-# Replace ZoneInfo usage with pytz
-API_KEY=os.getenv("API_KEY")
-EMAIL=os.getenv("EMAIL")
-PASS=os.getenv("PASS")
-
 def get_db_url():
-    # En développement, utilisez les variables d'environnement locales
-    # En production sur Render, utilisez l'URL de la base de données fournie
     return os.getenv('DB_URL')
 
 def get_connection():
-    # Obtenir l'URL de la base de données
     database_url = get_db_url()
     
     if not database_url:
@@ -34,5 +26,4 @@ def get_connection():
     return psycopg2.connect(database_url)
 
 def get_engine():
-    # Pour les opérations plus complexes nécessitant SQLAlchemy
     return create_engine(get_db_url())
